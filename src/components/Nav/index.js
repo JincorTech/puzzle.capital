346cfa1f8da15d6bbab2aa2b9806cc6cce3070dd
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-02-24 05:07:02 +0800

    Add aside

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import namedRoutes from '../../namedRoutes';
 
-export default () => (
-  <nav>
-    <Link to={namedRoutes.about}>about</Link>
-    <Link to={namedRoutes.strategy}>strategy</Link>
-    <Link to={namedRoutes.management}>management</Link>
-    <Link to={namedRoutes.contacts}>contacts</Link>
-    <Link to={namedRoutes.legal}>legal</Link>
-    <Link to="/e404">empty spaces</Link>
-  </nav>
-);
+import s from './styles.css';
+
+const Nav = () => {
+  const renderLink = (to, text) => (
+    <NavLink
+      exact
+      to={to}
+      className={s.link}
+      activeClassName={s.activeLink}>
+      {text}
+    </NavLink>
+  );
+
+  return (
+    <div className={s.aside}>
+      <div className={s.logo}>
+        <img src={require('../../assets/images/logo.svg')}/>
+      </div>
+
+      <nav className={s.nav}>
+        {renderLink(namedRoutes.about, 'About')}
+        {renderLink(namedRoutes.strategy, 'Strategy')}
+        {renderLink(namedRoutes.management, 'Management')}
+        {renderLink(namedRoutes.contacts, 'Contacts')}
+        {renderLink(namedRoutes.legal, 'Legal')}
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
