3dee9199a7bbde832e8930460bd0b5c1aa018db5
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-02-24 05:17:07 +0800

    Closes #1 Add footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,4 +2,15 @@ import React from 'react';
 
 import s from './styles.css';
 
-export default () => (<footer className={s.footer}/>);
+const Footer = () => (
+  <footer className={s.footer}>
+    <div>
+      © Copyright 2018 Puzzle Capital. All Rights Reserved.
+    </div>
+    <div>
+      British Virgin Islands (BVI), OMC Chambers, Wickhams Cay 1
+    </div>
+  </footer>
+);
+
+export default Footer;
