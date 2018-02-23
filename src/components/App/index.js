7e506a229b007be707f1a9d6a053bb0e2d6ca006
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-02-24 03:05:14 +0800

    Simpliest routing

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { Switch, Route, withRouter } from 'react-router-dom';
 
-import { namedRoutes } from '../../routes';
+import namedRoutes from '../../namedRoutes';
 
 import Nav from '../Nav';
-import Dashboard from '../Dashboard';
-import Settings from '../Settings';
+import About from '../About';
+import Strategy from '../Strategy';
+import Management from '../Management';
+import Contacts from '../Contacts';
+import Legal from '../Legal';
+import E404 from '../errors/E404';
 
 import s from './styles.css';
 
 const App = () => (
-  <div className={s.layout}>
-    <Nav/>
-    <Switch>
-      <Route exact path={namedRoutes.dashboard} component={Dashboard}/>
-      <Route exact path={namedRoutes.settings} component={Settings}/>
-      <Redirect from={namedRoutes.app} to={namedRoutes.dashboard}/>
-    </Switch>
+  <div className={s.container}>
+    <div>
+      <aside className={s.aside}>
+        <Nav/>
+      </aside>
+      <main className={s.main}>
+        <Switch>
+          <Route exact path={namedRoutes.about} component={About}/>
+          <Route exact path={namedRoutes.strategy} component={Strategy}/>
+          <Route exact path={namedRoutes.management} component={Management}/>
+          <Route exact path={namedRoutes.contacts} component={Contacts}/>
+          <Route exact path={namedRoutes.legal} component={Legal}/>
+          <Route exact path="*" component={E404}/>
+        </Switch>
+      </main>
+    </div>
+    <div className={s.footer}/>
   </div>
 );
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { namedRoutes } from '../../routes';
+import namedRoutes from '../../namedRoutes';
 
 export default () => (
   <nav>
-    <Link to={namedRoutes.dashboard}>Dashboard</Link>
-    <Link to={namedRoutes.settings}>Settings</Link>
+    <Link to={namedRoutes.about}>about</Link>
+    <Link to={namedRoutes.strategy}>strategy</Link>
+    <Link to={namedRoutes.management}>management</Link>
+    <Link to={namedRoutes.contacts}>contacts</Link>
+    <Link to={namedRoutes.legal}>legal</Link>
+    <Link to="/e404">empty spaces</Link>
   </nav>
 );
