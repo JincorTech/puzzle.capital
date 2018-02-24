d86dd1381fcac0511d3b1f225c8cc64f9c38cb52
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-02-25 02:43:22 +0800

    Closes #4 Add contact page

--- src/containers/Contacts/index.js
+++ src/containers/Contacts/index.js
@@ -8,6 +8,28 @@ const Contacts = () => (
   <div className={s.container}>
     <div className={s.title}>Contact Us</div>
     <ContactForm/>
+
+    <div className={s.links}>
+      <div className={s.link}>
+        <h4>General Inquiries</h4>
+        <a href="mailto:info@puzzle.capital">info@puzzle.capital</a>
+      </div>
+
+      <div className={s.link}>
+        <h4>Partnerships</h4>
+        <a href="mailto:partner@puzzle.capital">partner@puzzle.capital</a>
+      </div>
+
+      <div className={s.link}>
+        <h4>Investments</h4>
+        <a href="mailto:invest@puzzle.capital">invest@puzzle.capital</a>
+      </div>
+
+      <div className={s.link}>
+        <h4>Media/Press</h4>
+        <a href="mailto:press@puzzle.capital">press@puzzle.capital</a>
+      </div>
+    </div>
   </div>
 );
 
