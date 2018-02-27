51f72018a93927b4449139f9ae64633a5ad07b92
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-02-28 00:41:02 +0800

    Reset contact form state on unmount

--- src/containers/Contacts/index.js
+++ src/containers/Contacts/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { submitContactForm, resetStore } from '../../redux/modules/contactForm';
@@ -8,47 +8,53 @@ import ContactFormSuccess from '../../components/ContactFormSuccess';
 
 import s from './styles.css';
 
-const Contacts = (props) => {
-  const renderStep = () => {
-    switch (props.step) {
-      case 'contactForm':
-        return <ContactForm onSubmit={submitContactForm}/>;
-      case 'success':
-        return <ContactFormSuccess resetStore={props.resetStore}/>;
-      default:
-        return <div>Something went wrong.</div>;
-    }
-  };
+class Contacts extends Component {
+  componentWillUnmount() {
+    this.props.resetStore();
+  }
 
-  return (
-    <div className={s.container}>
-      <div className={s.title}>Contact Us</div>
-      {renderStep()}
+  render() {
+    const renderStep = () => {
+      switch (this.props.step) {
+        case 'contactForm':
+          return <ContactForm onSubmit={submitContactForm}/>;
+        case 'success':
+          return <ContactFormSuccess resetStore={this.props.resetStore}/>;
+        default:
+          return <div>Something went wrong.</div>;
+      }
+    };
 
-      {/* <div className={s.links}>
-        <div className={s.link}>
-          <h4>General Inquiries</h4>
-          <a href="mailto:info@puzzle.capital">info@puzzle.capital</a>
-        </div>
+    return (
+      <div className={s.container}>
+        <div className={s.title}>Contact Us</div>
+        {renderStep()}
 
-        <div className={s.link}>
-          <h4>Partnerships</h4>
-          <a href="mailto:partner@puzzle.capital">partner@puzzle.capital</a>
-        </div>
+        {/* <div className={s.links}>
+          <div className={s.link}>
+            <h4>General Inquiries</h4>
+            <a href="mailto:info@puzzle.capital">info@puzzle.capital</a>
+          </div>
 
-        <div className={s.link}>
-          <h4>Investments</h4>
-          <a href="mailto:invest@puzzle.capital">invest@puzzle.capital</a>
-        </div>
+          <div className={s.link}>
+            <h4>Partnerships</h4>
+            <a href="mailto:partner@puzzle.capital">partner@puzzle.capital</a>
+          </div>
 
-        <div className={s.link}>
-          <h4>Media/Press</h4>
-          <a href="mailto:press@puzzle.capital">press@puzzle.capital</a>
-        </div>
-      </div> */}
-    </div>
-  );
-};
+          <div className={s.link}>
+            <h4>Investments</h4>
+            <a href="mailto:invest@puzzle.capital">invest@puzzle.capital</a>
+          </div>
+
+          <div className={s.link}>
+            <h4>Media/Press</h4>
+            <a href="mailto:press@puzzle.capital">press@puzzle.capital</a>
+          </div>
+        </div> */}
+      </div>
+    );
+  }
+}
 
 export default connect(
   (state) => ({ ...state.contactForm }),
